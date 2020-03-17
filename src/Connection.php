8daa8d790c28d634c02b4e1b185ca5b97c482b33
Abyr Valg <valga.github@abyrga.ru>
2020-03-17 07:29:25 +0300

    Remove unnecessary override

--- src/Connection.php
+++ src/Connection.php
@@ -11,28 +11,4 @@ use BinSoul\Net\Mqtt\Message;
 interface Connection extends BaseConnection
 {
     public function toThrift(): string;
-
-    public function getProtocol(): int;
-
-    public function getClientID(): string;
-
-    public function isCleanSession(): bool;
-
-    public function getUsername(): string;
-
-    public function getPassword(): string;
-
-    public function getWill(): ?Message;
-
-    public function getKeepAlive(): int;
-
-    public function withProtocol(int $protocol): BaseConnection;
-
-    public function withClientID(string $clientID): BaseConnection;
-
-    public function withCredentials(string $username, string $password): BaseConnection;
-
-    public function withWill(Message $will = null): BaseConnection;
-
-    public function withKeepAlive(int $timeout): BaseConnection;
 }
